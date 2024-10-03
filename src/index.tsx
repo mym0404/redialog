f66478946e8b14c0aa577a6bf19ba467fd664727
Mystic <mym0404@gmail.com>
2024-10-04 00:13:44 +0900

    chore: unconscious commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
 export * from './component/Dialog';
-export { PortalHost as DialogPortalHost } from './internal/Portal';
+export { PortalHost, Portal } from './internal/Portal';
 export * from './hook/useDialog';
 export * from './hook/useDialogProps';
