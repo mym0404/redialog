561cce7ceedbcbbb7984ad8b63fcf71f0edb119b
Mystic <mym0404@gmail.com>
2024-10-04 02:02:51 +0900

    chore: unconscious commit

--- src/internal/type.ts
+++ src/internal/type.ts
@@ -2,9 +2,9 @@ import type { RefObject, ComponentType, ComponentProps } from 'react';
 
 export type ExtractShowFromComponent<
   T extends ComponentType<{
-    dialog: RefObject<any>;
+    dialog?: RefObject<any>;
   }>,
-> = NonNullable<ComponentProps<T>['dialog']['current']>['show'];
+> = NonNullable<NonNullable<ComponentProps<T>['dialog']>['current']>['show'];
 
 const _NoobSymbol = Symbol('noob');
 export type NoobSymbol = typeof _NoobSymbol;
